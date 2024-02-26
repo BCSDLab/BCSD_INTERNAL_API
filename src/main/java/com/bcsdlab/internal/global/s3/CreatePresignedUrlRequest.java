535baa4482545944b3ba8fa1a736e3dc16910b71
송선권 <songsunkook@gmail.com>
2024-02-26 09:10:29 +0900

    fix: 이미지 파일명에 공백 포함 가능하도록 수정 (#4)
    * fix: 이미지 파일명에 공백 포함 가능하도록 수정
    
    * docs: gitignore 수정

--- src/main/java/com/bcsdlab/internal/global/s3/CreatePresignedUrlRequest.java
+++ src/main/java/com/bcsdlab/internal/global/s3/CreatePresignedUrlRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record CreatePresignedUrlRequest(
     @Schema(description = "사용 가능한 확장자: jpg|gif|png|jpeg|webp", example = "image.png")
-    @Pattern(regexp = "(\\S+(?=\\.(jpg|gif|png|jpeg|webp))\\.\\2)")
+    @Pattern(regexp = "(\\S[\\s\\S]*\\S(?=\\.(jpg|gif|png|jpeg|webp))\\.\\2)")
     String fileName
 ) {
 
