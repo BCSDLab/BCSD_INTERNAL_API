7ba517ce1e2db61af9683c8e7c780166bcabd759
seongjae6751 <seongjae6751@naver.com>
2024-04-06 21:47:24 +0900

    fix: 시작 시간이 겹쳐서 예약 정보 수정 안되는 에러 수정

--- src/main/java/com/bcsdlab/internal/reservation/repository/ReservationCustomRepositoryImpl.java
+++ src/main/java/com/bcsdlab/internal/reservation/repository/ReservationCustomRepositoryImpl.java
@@ -31,11 +31,12 @@ public class ReservationCustomRepositoryImpl implements ReservationCustomReposit
     @Override
     public boolean isExistReservationNotId(LocalDateTime startDateTime, LocalDateTime endDateTime, Long id) {
         LocalDateTime adjustedEndDateTime = endDateTime.minusSeconds(1);
+        LocalDateTime adjustedStartDateTime = startDateTime.plusSeconds(1);
 
         int existCount = queryFactory.select(reservation)
             .from(reservation)
-            .where((reservation.startDateTime.between(startDateTime, adjustedEndDateTime)
-                .or(reservation.endDateTime.between(startDateTime, adjustedEndDateTime))
+            .where((reservation.startDateTime.between(adjustedStartDateTime, adjustedEndDateTime)
+                .or(reservation.endDateTime.between(adjustedStartDateTime, adjustedEndDateTime))
                 .and(reservation.id.ne(id))))
             .fetch()
             .size();
