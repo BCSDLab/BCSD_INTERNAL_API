896d9ebfc8661140eb1f206741fb68a524a99ac0
HyeonsuLee <leehyeonsu4888@naver.com>
2024-07-20 01:04:41 +0900

    feat: 테스트용 커멘드설정

--- src/main/java/com/bcsdlab/internal/global/slack/config/SlackAppConfig.java
+++ src/main/java/com/bcsdlab/internal/global/slack/config/SlackAppConfig.java
@@ -1,82 +1,80 @@
 package com.bcsdlab.internal.global.slack.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
-import com.slack.api.model.event.MessageEvent;
+import com.slack.api.bolt.socket_mode.SocketModeApp;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
 @Configuration
 public class SlackAppConfig {
 
     private final String SLACK_BOT_TOKEN;
+    private final String SLACK_APP_TOKEN;
     private final String SLACK_BOT_SIGNING_SECRET;
     private final String SLACK_BOT_CLIENT_ID;
     private final String SLACK_BOT_CLIENT_SECRET;
 
     public SlackAppConfig(
         @Value("${slack.bot-token}") String botToken,
+        @Value("${slack.app-token}") String appToken,
         @Value("${slack.signing-secret}") String signingSecret,
         @Value("${slack.client-id}") String clientId,
         @Value("${slack.client-secret}") String clientSecret
     ) {
         this.SLACK_BOT_TOKEN = botToken;
+        this.SLACK_APP_TOKEN = appToken;
         this.SLACK_BOT_SIGNING_SECRET = signingSecret;
         this.SLACK_BOT_CLIENT_ID = clientId;
         this.SLACK_BOT_CLIENT_SECRET = clientSecret;
     }
 
-    // If you would like to run this app for a single workspace,
-    // enabling this Bean factory should work for you.
     @Bean
-    public AppConfig loadSingleWorkspaceAppConfig() {
-        return AppConfig.builder()
-            .singleTeamBotToken(SLACK_BOT_TOKEN)
-            .signingSecret(SLACK_BOT_SIGNING_SECRET)
-            .build();
-    }
+    public App initSlackApp() {
+        App app = new App();
 
-    // If you would like to run this app for multiple workspaces,
-    // enabling this Bean factory should work for you.
-    // @Bean
-    // public AppConfig loadOAuthConfig() {
-    //     return AppConfig.builder()
-    //         .singleTeamBotToken(null)
-    //         .clientId(SLACK_BOT_CLIENT_ID)
-    //         .clientSecret(SLACK_BOT_CLIENT_SECRET)
-    //         .signingSecret(SLACK_BOT_SIGNING_SECRET)
-    //         .scope("app_mentions:read,channels:history,channels:read,chat:write")
-    //         .oauthInstallPath("/slack/install")
-    //         .oauthRedirectUriPath("/slack/oauth_redirect")
-    //         .build();
-    // }
-
-    @Bean
-    public App initSlackApp(AppConfig config) {
-        App app = new App(config).asOAuthApp(true);
-        if (config.getClientId() != null) {
-            app.asOAuthApp(true);
-        }
         app.command("/스프링부트테스트", (req, ctx) -> {
-            return ctx.ack("What's up?");
+            System.out.println("스프링부트환경테스트");
+
+            // 커맨드 입력한 사용자에게 응답
+            ctx.ack("스프링부트환경테스트");
+
+            // 채널에 메시지 전송
+            String channelId = req.getPayload().getChannelId();
+
+            MethodsClient client = ctx.client();
+
+            try {
+                ChatPostMessageResponse response = client.chatPostMessage(r -> r
+                    .token(SLACK_BOT_TOKEN)
+                    .channel(channelId)
+                    .text("Spring Boot에서 볼트앱 연결 성공했다ㅋㅋ"));
+
+                if (!response.isOk()) {
+                    System.err.println("Error posting message: " + response.getError());
+                }
+
+            } catch (IOException | SlackApiException e) {
+                e.printStackTrace();
+            }
+
+            return ctx.ack();
         });
+
         return app;
-        // app.command(MessageEvent.class, (payload, ctx) -> {
-        //     String text = payload.getEvent().getText();
-        //     // 특정 키워드 또는 조건에 따라 응답
-        //     System.out.println("dd");
-        //     if (text.contains("hello")) {
-        //         System.out.println("dsa");
-        //         ctx.say("Hello! How can I assist you today?");
-        //     } else if (text.contains("help")) {
-        //         ctx.say("Sure, what do you need help with?");
-        //     }
-        //     return ctx.ack();
-        // });
-        //
-        // return app;
     }
 
+    @Bean
+    public SocketModeApp socketModeApp(App app) throws Exception {
+        SocketModeApp socketModeApp = new SocketModeApp(SLACK_APP_TOKEN, app);
+        socketModeApp.start();
+        return socketModeApp;
+    }
 }
