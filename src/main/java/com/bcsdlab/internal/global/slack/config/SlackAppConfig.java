830c50b0ff7427440f04c5c6a72c252196bab7e4
HyeonsuLee <leehyeonsu4888@naver.com>
2024-08-15 19:09:42 +0900

    test

--- src/main/java/com/bcsdlab/internal/global/slack/config/SlackAppConfig.java
+++ src/main/java/com/bcsdlab/internal/global/slack/config/SlackAppConfig.java
@@ -33,7 +33,7 @@ public class SlackAppConfig {
     //     app.event(MessageEvent.class, (payload, ctx) -> {
     //         MessageEvent event = payload.getEvent();
     //         if (!event.getText().equals("hello!")) return ctx.ack();
-    //
+
     //         // 메시지 내용 처리 로직
     //         try {
     //             ctx.client().chatPostMessage(r -> r
