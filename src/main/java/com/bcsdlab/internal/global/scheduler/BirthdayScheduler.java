dc8e84a9dde1fe4556c743a7c6ba0be4974c29f2
dradnats1012 <herjebi1012@gmail.com>
2025-03-13 00:16:00 +0900

    rollback: 생일축하 자정으로 다시 변경

--- src/main/java/com/bcsdlab/internal/global/scheduler/BirthdayScheduler.java
+++ src/main/java/com/bcsdlab/internal/global/scheduler/BirthdayScheduler.java
@@ -13,7 +13,7 @@ public class BirthdayScheduler {
 
     private final MemberService memberService;
 
-    @Scheduled(cron = "0 0 8 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void celebrateBirthday() {
         memberService.celebrateBirthday();
     }
